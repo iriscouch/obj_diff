a3739079feed990b78c66259d084f7ffc7e1bb7b
Jason Smith (work) <jhs@iriscouch.com>
2011-11-07 05:33:16 +0000

    Convert to tap

--- t/diffs.js
+++ t/diffs.js
@@ -1,42 +1,50 @@
-var util = require('util')
-  , assert = require('assert')
+// obj_diff API tests
+//
+// Copyright 2011 Iris Couch
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+var test = require('tap').test
   , obj_diff = require('../api')
-  , JS = JSON.stringify
+  ;
+
+var JS = JSON.stringify
   , JP = JSON.parse
   , JDUP = function(x) { return JSON.parse(JSON.stringify(x)) }
   ;
 
-var known = [
-  [{}, {}, {}]
-, [{a:1}, {a:1}, {}]
-, [{}, {foo:'bar'}, {foo: {from:undefined, to:'bar'}}]
-, [{foo:'bar'}, {}, {foo: {from:'bar', to:undefined}}],
-, [{a:1}, {a:"1"}, {a: {from:1, to:"1"}}]
-, [{a:1}, {b:1}, {a: {from:1, to:undefined}, b: {from:undefined, to:1}}]
-, [{}, {obj:{hi:true}}, {obj: {from:undefined, to:{hi:true}}}]
-, [{first:{second:{value:false}}}, {first:{second:{value:true}}}, {first:{second:{value:{from:false, to:true}}}}]
-]
 
-function main() {
-  var tests = 0;
+test('Defaults API', function(t) {
+  var known =
+  [ [{}, {}, {}]
+  , [{a:1}, {a:1}, {}]
+  , [{}, {foo:'bar'}, {foo: {from:undefined, to:'bar'}}]
+  , [{foo:'bar'}, {}, {foo: {from:'bar', to:undefined}}],
+  , [{a:1}, {a:"1"}, {a: {from:1, to:"1"}}]
+  , [{a:1}, {b:1}, {a: {from:1, to:undefined}, b: {from:undefined, to:1}}]
+  , [{}, {obj:{hi:true}}, {obj: {from:undefined, to:{hi:true}}}]
+  , [{first:{second:{value:false}}}, {first:{second:{value:true}}}, {first:{second:{value:{from:false, to:true}}}}]
+  ];
+
   known.forEach(function(row) {
     var from = row[0]
       , to   = row[1]
       , expected = row[2];
 
     var diff = obj_diff.diff(from, to);
-    assert.deepEqual(diff, expected, "\n" + [ JS(from)
-                                            , "vs"
-                                            , JS(to)
-                                            , "= " + JS(diff)
-                                            , "expected: " + JS(expected)
-                                            ].join('\n'));
-    tests += 1;
+    var message = "from="+JS(from) + " to="+JS(to);
+    t.same(diff, expected, message);
   })
 
-  console.log('diff tests passed: ' + tests);
-}
-
-exports.main = main;
-if(require.main === module)
-  main();
+  t.end();
+})
--- t/policy.js
+++ t/policy.js
@@ -1,5 +1,26 @@
-var assert = require('assert')
-  , obj_diff = require('../diff')
+// obj_diff policy tests
+//
+// Copyright 2011 Iris Couch
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+var test = require('tap').test
+  , obj_diff = require('../api')
+  ;
+
+var JS = JSON.stringify
+  , JP = JSON.parse
+  , JDUP = function(x) { return JSON.parse(JSON.stringify(x)) }
   ;
 
 var fixtures = {
@@ -16,8 +37,10 @@ var fixtures = {
   }
 }
 
-var tests = 0;
-var go = function(method, from_id, to_id, policy, expected) {
+function make_tester(t) {
+  return tester;
+
+function tester(method, from_id, to_id, policy, expected) {
   var from_merge = {}, to_merge = {};
 
   if(typeof from_id == 'object') {
@@ -34,39 +57,32 @@ var go = function(method, from_id, to_id, policy, expected) {
   var from = JSON.parse(JSON.stringify(fixtures[from_id]));
   var to   = JSON.parse(JSON.stringify(fixtures[to_id]));
 
-  for(var k in from_merge) {
+  var k;
+  for(k in from_merge) {
     if(from_merge[k] === undefined)
       delete from[k];
     else
       from[k] = from_merge[k];
   }
-  for(var k in to_merge) {
+
+  for(k in to_merge) {
     if(to_merge[k] === undefined)
       delete to[k];
     else
       to[k] = to_merge[k];
   }
 
-  try {
-    var result = obj_diff[method](from, to, policy);
-  } finally {
-    tests = tests + 1;
-  }
+  method = method.replace(/^doc_diff_/, "");
+  var result = obj_diff[method](from, to, policy);
+  var message = "from="+JS(from) + " to="+JS(to) + " policy="+JS(policy);
 
-  var message = [ method
-                , JSON.stringify(from)
-                , 'vs.'
-                , JSON.stringify(to)
-                , 'with policy ' + JSON.stringify(policy)
-                , 'should be ' + expected + ' but was ' + result
-                ].join("\n");
-  assert.equal(result, expected, message);
+  return t.same(result, expected, message);
 }
 
-function main() {
-  //
-  // atmost
-  //
+}
+
+test('At most', function(t) {
+  var go = make_tester(t);
 
   go('doc_diff_atmost', 'server', 'server', {}, true);
   go('doc_diff_atmost', 'server', 'server', {}, true);
@@ -100,9 +116,11 @@ function main() {
   go('doc_diff_atmost', 'transfer', {base:'transfer'}, {transfer:{to:{}}}, true);
   go('doc_diff_atmost', 'transfer', {base:'transfer', transfer:{to:null}}, {transfer:{nest:true, to:{}}}, true);
 
-  //
-  // atleast
-  //
+  t.end();
+})
+
+test('At least', function(t) {
+  var go = make_tester(t);
 
   go('doc_diff_atleast', 'server', 'server', {}, true);
   go('doc_diff_atleast', 'server', 'server', {change: {}}, false);
@@ -118,14 +136,9 @@ function main() {
   go('doc_diff_atleast', 'transfer', {base:'transfer', transfer:{to:23}}, {transfer:{nest:true, to:{}}}, true);
   go('doc_diff_atleast', 'transfer', {base:'transfer', transfer:{to:null}}, {transfer:{nest:true, to:{}}}, true);
 
-  assert.throws(function() {
+  t.throws(function() {
     go('doc_diff_atmost', {base:'server', reboot:false}, 'server', {reboot:{from:undefined, to:undefined}}, true);
-  }, Error)
+  }, 'Undefined from and to')
 
-  console.log('Policy tests passed: ' + tests);
-}
-
-
-exports.main = main;
-if(require.main === module)
-  main();
+  t.end();
+})
