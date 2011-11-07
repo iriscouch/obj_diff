7709cf4cae3717f92f4694977c3a43025b559a5f
Jason Smith <jhs@iriscouch.com>
2011-11-07 08:53:57 +0700

    Convert to defaultable

--- api.js
+++ api.js
@@ -1,10 +1,27 @@
-var util = require('util')
-  , diff = require('./diff')
-  ;
+// The obj_diff API
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+require('defaultable').def(module,
+  {
+  }, function(module, exports, DEFS, require) {
+
+var diff = require('./diff');
 var API = { 'diff'   : diff.doc_diff
           , 'atmost' : diff.doc_diff_atmost
           , 'atleast': diff.doc_diff_atleast
           };
 
 module.exports = API;
+
+}) // defaultable
